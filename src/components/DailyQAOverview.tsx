10671a118eb36341341e4b315a33049e7e419912
Christopher Liu <ccccrizzzz@gmail.com>
2024-02-29 19:02:07 -0500

    added daily shelf sheet CSV download function

--- src/components/DailyQAOverview.tsx
+++ src/components/DailyQAOverview.tsx
@@ -3,8 +3,9 @@ import axios, { AxiosError, AxiosResponse } from "axios"
 import { Title, Button } from "@tremor/react"
 import { server } from "../utils/utils"
 import { ClipLoader } from "react-spinners"
-import { FaRotate } from "react-icons/fa6"
+import { FaFileCsv, FaRotate } from "react-icons/fa6"
 import { Table } from 'react-bootstrap'
+import moment from "moment"
 
 // Daily QA record count overview table 
 const DailyQAOverview = () => {
@@ -34,6 +35,33 @@ const DailyQAOverview = () => {
     setIsLoading(false)
   }
 
+  const exportToday = async () => {
+    await axios({
+      method: 'get',
+      url: server + '/inventoryController/getAllShelfSheet',
+      responseType: 'blob',
+      timeout: 8000,
+      withCredentials: true
+    }).then(async (res: AxiosResponse) => {
+      if (res.status > 200) return alert('No records found for today!')
+
+      // JSON parse csv content
+      let file = new Blob([res.data], { type: 'text/csv' })
+      const csv = JSON.parse(await file.text())
+      file = new Blob([csv], { type: 'text/csv' })
+
+      // way to get custom file name
+      let link = document.createElement("a");
+      link.setAttribute("href", URL.createObjectURL(file));
+      link.setAttribute("download", `${moment().format('LL')}_Shelf_Sheet.csv`);
+      document.body.appendChild(link);
+      link.click()
+      document.body.removeChild(link)
+    }).catch((res: AxiosError) => {
+      alert('Cannot Get Shelf Sheet CSV: ' + res.message)
+    })
+  }
+
   const renderTableHead = () => datesArr.reverse().map((val, i) => (
     <th key={i}>{val}</th>
   ))
@@ -53,7 +81,22 @@ const DailyQAOverview = () => {
     <>
       <div className="flex">
         <Title>📆 Past 7 Days QA Records</Title>
-        <Button className="absolute right-16" color="emerald" onClick={getData}><FaRotate /></Button>
+        <Button
+          className="absolute right-36"
+          color="emerald"
+          onClick={exportToday}
+          tooltip="Get Today's Shelf Sheet CSV"
+        >
+          <FaFileCsv />
+        </Button>
+        <Button
+          className="absolute right-16"
+          color="emerald"
+          onClick={getData}
+          tooltip="Refresh Table"
+        >
+          <FaRotate />
+        </Button>
       </div>
       {
         isLoading ?
